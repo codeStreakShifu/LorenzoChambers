7f5f171d5e603dcdb7a17d649e44a68c8b0f7bf9
codeStreakShifu <undertaker0000aw@gmail.com>
2024-11-27 22:43:07 +0800

    AOS

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// Select all links in the head_links div
-const links = document.querySelectorAll('.head_links a');
-
-// Loop through links and add a click event listener to each
-links.forEach(link => {
-    link.addEventListener('click', function() {
-        // Remove active class from all links
-        links.forEach(l => l.classList.remove('active'));
-        // Add active class to the clicked link
-        this.classList.add('active');
-    });
-});
 
 document.addEventListener('DOMContentLoaded', () => {
-    AOS.init();
+    AOS.init({
+        duration: 600,
+        offset: 100,
+      });
   });
 
-
-  // Hamburger menu toggle
-document.getElementById('hamburger').addEventListener('click', function() {
-    const navbar = document.getElementById('head_links');
-    navbar.classList.toggle('active'); // Toggle the active class on click
-});
